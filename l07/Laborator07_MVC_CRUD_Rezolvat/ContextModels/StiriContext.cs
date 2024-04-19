be447906495ee1a01da9a7f982bd1b29a84950a9
DanielW98 <wagner.daniel98@yahoo.com>
2024-04-19 21:15:03 +0300

    Lab07: MVC + CRUD SQLServer (important pt proiect si examen)

---
+++ l07/Laborator07_MVC_CRUD_Rezolvat/ContextModels/StiriContext.cs
@@ -0,0 +1,13 @@
+﻿using Laborator07.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Laborator07.ContextModels;
+
+public class StiriContext : DbContext
+{
+    public StiriContext(DbContextOptions<StiriContext> options) : base(options)
+    {
+    }
+    public DbSet<StireModel> Stire { get; set; }
+    public DbSet<CategorieModel> Categorie { get; set; }
+}
---
+++ l07/Laborator07_MVC_CRUD_Rezolvat/Models/CategorieModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Laborator07.Models;
+
+public class CategorieModel
+{
+    public int Id { get; set; }
+    public string Nume { get; set; }
+}
