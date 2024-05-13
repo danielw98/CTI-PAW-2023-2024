f42a84e7082df7e7fccd19df201d69f463ee99ae
DanielW98 <wagner.daniel98@yahoo.com>
2024-05-13 18:49:00 +0300

    Adaugat: Link-uri resurse WebAPI si AutoMapper

---
+++ l10/links_WebAPI.bat
@@ -0,0 +1,6 @@
+start chrome "https://geeksarray.com/blog/how-to-use-automapper-in-aspnet-core-web-api" ^
+      "https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-8.0&tabs=visual-studio" ^
+      "https://www.c-sharpcorner.com/article/asp-net-core-6-web-api-crud-with-entity-framework/" ^
+      "https://dev.to/moe23/net-6-automapper-data-transfer-objects-dtos-49e" ^
+      "https://medium.com/net-core/build-a-restful-web-api-with-asp-net-core-6-30747197e229" ^
+      "https://learn.microsoft.com/en-us/aspnet/core/security/authentication/social/?view=aspnetcore-6.0&tabs=visual-studio"
