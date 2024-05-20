37f60ddc5569c91818354ce1b01fe23190ea3169
DanielW98 <wagner.daniel98@yahoo.com>
2024-05-20 15:30:28 +0300

    Update: Lab10 - StireModelUI with ID's in API

--- l10/lab10/Models.UI/StireModelUI.cs
+++ l10/lab10/Models.UI/StireModelUI.cs
@@ -2,8 +2,10 @@
 
 public class StireModelUI
 {
+    public int Id { get; set; }
     public string Titlu { get; set; }
     public string Lead { get; set; }
     public string Continut { get; set; }
     public string Autor { get; set; }
+    public int CategorieId { get; set; }
 }
